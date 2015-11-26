d7d6ec89ab85cea576834e19344678df45724dab
Divye Kapoor <divyekapoor@gmail.com>
2015-11-25 16:46:49 -0800

    Update the install command for .aliases.

--- install.sh
+++ install.sh
@@ -8,13 +8,12 @@ echo "Running :PluginInstall"
 vim -S install.vim
 echo "PluginInstall done."
 
-if [ ! -f ~/.aliases ]; then
-  echo "Copying .aliases"
-  cp .aliases ~/.aliases
-  if [ -f ~/.bash_aliases ]; then
-    echo "Adding to .bash_aliases"
-    echo . ~/.aliases >> ~/.bash_aliases
-  fi
+echo "Installing .aliases."
+cp -i .aliases ~/.aliases
+
+if [ -f ~/.bash_aliases ]; then
+  echo "Adding to .bash_aliases"
+  echo ". ~/.aliases" >> ~/.bash_aliases
 fi
 
 echo "Please source your .bashrc to continue."

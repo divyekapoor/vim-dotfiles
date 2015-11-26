ff91f44b97838ea49932922e39140678feb8b436
Divye Kapoor <divyekapoor@gmail.com>
2015-11-25 16:32:45 -0800

    Update Installation Script
    - Update Vundle install codes.
    - Copy .aliases on the system if it doesn't already exist.

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
 echo "Cloning Vundle's git repo."
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
 echo "Install .vimrc"
 cp .vimrc $HOME/
-echo "Running :BundleInstall"
+echo "Running :PluginInstall"
 vim -S install.vim
+echo "PluginInstall done."
+
+if [ ! -f ~/.aliases ]; then
+  echo "Copying .aliases"
+  cp .aliases ~/.aliases
+  echo "Adding to .bash_aliases"
+  if [ -f ~/.bash_aliases ]; then
+    echo . ~/.aliases >> ~/.bash_aliases
+  fi
+fi
+
 echo "Done"

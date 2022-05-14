c4223dd9c6f27583a8083cc342ab2b23bfd9e891
Divye Kapoor <divyekapoor@gmail.com>
2022-05-14 09:05:56 +0000

    Reset before installing.

--- portainer_install.sh
+++ portainer_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo docker volume rm portainer_data
+sudo docker rm portainer
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest

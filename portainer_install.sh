63374b9089fb526959fd902838ac6fdc4170350c
Divye Kapoor <divyekapoor@gmail.com>
2022-05-14 09:09:03 +0000

    Fix install for portainer.

--- portainer_install.sh
+++ portainer_install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#
+# Install Portainer on an Proxmox Virtual Container (LXC).
+# Requires Privileged Container
 
-sudo docker volume rm portainer_data
+sudo apt remove apparmor
+sudo docker stop portainer
 sudo docker rm portainer
+sudo docker volume rm portainer_data
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest

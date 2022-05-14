7ef5fcfcc7434f98be82f072dbfce6655fae6986
Divye Kapoor <divyekapoor@gmail.com>
2022-05-14 08:57:48 +0000

    Fix portainer sudo.

--- portainer_install.sh
+++ portainer_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
